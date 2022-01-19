bf466edcb84faae183f1c2752c4a77fbadcce3d5
josephzxy <josephchuh7@gmail.com>
2022-01-19 21:03:53 +0800

    ✨ feat: add models

--- py_v_sdk/__init__.py
+++ py_v_sdk/__init__.py
@@ -2,6 +2,7 @@ from py_v_sdk.api import *
 from py_v_sdk.chain import *
 from py_v_sdk.account import *
 from py_v_sdk.tx_req import *
+from py_v_sdk.model import *
 from py_v_sdk.contract import *
 from py_v_sdk.contract.nft_ctrt import *
 from py_v_sdk.contract.atomic_swap_ctrt import *
