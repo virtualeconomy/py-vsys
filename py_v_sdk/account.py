ce99de545a7a78b41403557c33addb959ba56469
josephzxy <josephchuh7@gmail.com>
2022-01-20 16:43:56 +0800

    📃 docs: add doc strings for account.py

--- py_v_sdk/account.py
+++ py_v_sdk/account.py
@@ -1,7 +1,8 @@
+"""
+account contains account-related resources
+"""
 from __future__ import annotations
 from typing import Any, Dict, TYPE_CHECKING
-
-import base58
 
 # https://stackoverflow.com/a/39757388
 if TYPE_CHECKING:
@@ -15,10 +16,19 @@ from py_v_sdk.utils.crypto import curve_25519 as curve
 
 
 class Account:
+    """
+    Account is a class for an account on the chain.
+    """
 
     ADDR_VER = 5
 
-    def __init__(self, chain: ch.Chain, seed: str, nonce: int = 0):
+    def __init__(self, chain: ch.Chain, seed: str, nonce: int = 0) -> None:
+        """
+        Args:
+            chain (ch.Chain): The chain that the account is on.
+            seed (str): The seed string of the account.
+            nonce (int, optional): The nonce of the account. Defaults to 0.
+        """
         self._chain = chain
         self._seed = md.Str(seed)
         self._nonce = md.Nonce(nonce)
@@ -30,48 +40,123 @@ class Account:
 
     @property
     def chain(self) -> ch.Chain:
+        """
+        chain returns the chain that the account is on.
+
+        Returns:
+            ch.Chain: The chain that the account is on.
+        """
         return self._chain
 
     @property
     def api(self) -> api.NodeAPI:
+        """
+        api returns the NodeAPI object that the account's chain uses.
+
+        Returns:
+            api.NodeAPI: The NodeAPI object that the account's chain uses.
+        """
         return self._chain.api
 
     @property
     def seed(self) -> str:
+        """
+        seed returns the account's seed string.
+
+        Returns:
+            str: The account's seed string.
+        """
         return self._seed.data
 
     @property
     def nonce(self) -> int:
+        """
+        nonce returns the account's nonce.
+
+        Returns:
+            int: The account's nonce.
+        """
         return self._nonce.data
 
     @property
     def acnt_seed_hash(self) -> md.Bytes:
+        """
+        acnt_seed_hash returns the account's account seed hash.
+
+        Returns:
+            md.Bytes: The account's account seed hash.
+        """
         return self._acnt_seed_hash
 
     @property
     def key_pair(self) -> md.KeyPair:
+        """
+        key_pair returns the account's key pair.
+
+        Returns:
+            md.KeyPair: The account's key pair.
+        """
         return self._key_pair
 
     @property
     def addr(self) -> md.Bytes:
+        """
+        addr returns the account's address.
+
+        Returns:
+            md.Bytes: The account's address.
+        """
         return self._addr
 
     @property
     def balance(self) -> int:
+        """
+        balance returns the account's balance.
+
+        Returns:
+            int: The account's balance.
+        """
         return self.api.addr.get_balance(self.addr.b58_str)["balance"]
 
     def register_contract(self, req: tx.RegCtrtTxReq) -> Dict[str, Any]:
+        """
+        register_contract sends a register contract transaction on behalf of the account.
+
+        Args:
+            req (tx.RegCtrtTxReq): The register contract transaction request.
+
+        Returns:
+            Dict[str, Any]: The response returned by the Node API.
+        """
         return self.api.ctrt.broadcast_register(
             req.to_broadcast_register_payload(self.key_pair)
         )
 
     def execute_contract(self, req: tx.ExecCtrtFuncTxReq) -> Dict[str, Any]:
+        """
+        execute_contract sends an execute contract transaction on behalf of the account.
+
+        Args:
+            req (tx.ExecCtrtFuncTxReq): The execute contract transaction request.
+
+        Returns:
+            Dict[str, Any]: The response returned by the Node API.
+        """
         return self.api.ctrt.broadcast_execute(
             req.to_broadcast_execute_payload(self.key_pair)
         )
 
     @staticmethod
     def get_key_pair(acnt_seed_hash: bytes) -> md.KeyPair:
+        """
+        get_key_pair generates a key pair based on the given account seed hash.
+
+        Args:
+            acnt_seed_hash (bytes): The account seed hash.
+
+        Returns:
+            md.KeyPair: The generated key pair.
+        """
         pri_key = curve.gen_pri_key(acnt_seed_hash)
         pub_key = curve.gen_pub_key(pri_key)
 
@@ -82,6 +167,18 @@ class Account:
 
     @staticmethod
     def get_addr(pub_key: bytes, addr_ver: int, chain_id: ch.ChainID) -> md.Bytes:
+        """
+        get_addr generates the address based on the given data.
+
+        Args:
+            pub_key (bytes): The public key.
+            addr_ver (int): The address version.
+            chain_id (ch.ChainID): The chain ID.
+
+        Returns:
+            md.Bytes: The generated address.
+        """
+
         def ke_bla_hash(b: bytes) -> bytes:
             return hs.keccak256_hash(hs.blake2b_hash(b))
 
@@ -96,6 +193,16 @@ class Account:
 
     @staticmethod
     def get_acnt_seed_hash(seed: str, nonce: int) -> md.Bytes:
+        """
+        get_acnt_seed_hash generates account seed hash based on the given seed & nonce.
+
+        Args:
+            seed (str): The account seed.
+            nonce (int): The account nonce.
+
+        Returns:
+            md.Bytes: The generated account seed hash.
+        """
         b = hs.sha256_hash(
             hs.keccak256_hash(hs.blake2b_hash(f"{nonce}{seed}".encode("latin-1")))
         )
