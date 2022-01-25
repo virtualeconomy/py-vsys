9cdf681db16559588db5574efcb6821b0a59b6cd
Peerchemist <dx.1178@gmail.com>
2022-01-25 14:38:03 +0100

    drop depenedency on base58

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setuptools.setup(
     install_requires=[
         "aiohttp~=3.8.1",
         "python-axolotl-curve25519~=0.4.1.post2",
-        "base58~=2.1.1",
         "loguru~=0.5.3",
     ],
     python_requires=">=3.7",
