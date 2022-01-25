f9cb2bfb8ba68513c8471e520f62d89697377980
Meiyunoda <73986634+Meiyunoda@users.noreply.github.com>
2022-01-25 15:41:52 +0800

    refactor: deposit and withdraw changed
    change sender and receipt to by.addr.b58_str

--- py_v_sdk/contract/tok_ctrt.py
+++ py_v_sdk/contract/tok_ctrt.py
@@ -14,16 +14,17 @@ from py_v_sdk import model as md
 
 from . import CtrtMeta, Ctrt
 
-class TokenCtrtWithoutSplit(Ctrt): 
+
+class TokenCtrtWithoutSplit(Ctrt):
     """
     TokenCtrtWithoutSplit is the class that encapsulates behaviours of the VSYS TOKEN contract without split v1.
     """
-    
+
     CTRT_META = CtrtMeta.from_b58_str(
         "3GQnJtxDQc3zFuUwXKbrev1TL7VGxk5XNZ7kUveKK6BsneC1zTSTRjgBTdDrksHtVMv6nwy9Wy6MHRgydAJgEegDmL4yx7tdNjdnU38b8FrCzFhA1aRNxhEC3ez7JCi3a5dgVPr93hS96XmSDnHYvyiCuL6dggahs2hKXjdz4SGgyiUUP4246xnELkjhuCF4KqRncUDcZyWQA8UrfNCNSt9MRKTj89sKsV1hbcGaTcX2qqqSU841HyokLcoQSgmaP3uBBMdgSYVtovPLEFmpXFMoHWXAxQZDaEtZcHPkrhJyG6CdTgkNLUQKWtQdYzjxCc9AsUGMJvWrxWMi6RQpcqYk3aszbEyAh4r4fcszHHAJg64ovDgMNUDnWQWJerm5CjvN76J2MVN6FqQkS9YrM3FoHFTj1weiRbtuTc3mCR4iMcu2eoxcGYRmUHxKiRoZcWnWMX2mzDw31SbvHqqRbF3t44kouJznTyJM6z1ruiyQW6LfFZuV6VxsKLX3KQ46SxNsaJoUpvaXmVj2hULoGKHpwPrTVzVpzKvYQJmz19vXeZiqQ2J3tVcSFH17ahSzwRkXYJ5HP655FHqTr6Vvt8pBt8N5vixJdYtfx7igfKX4aViHgWkreAqBK3trH4VGJ36e28RJP8Xrt6NYG2icsHsoERqHik7GdjPAmXpnffDL6P7NBfyKWtp9g9C289TDGUykS8CNiW9L4sbUabdrqsdkdPRjJHzzrb2gKTf2vB56rZmreTUbJ53KsvpZht5bixZ59VbCNZaHfZyprvzzhyTAudAmhp8Nrks7SV1wTySZdmfLyw7vsNmTEi3hmuPmYqExp4PoLPUwT4TYt2doYUX1ds3CesnRSjFqMhXnLmTgYXsAXvvT2E6PWTY5nPCycQv5pozvQuw1onFtGwY9n5s2VFjxS9W6FkCiqyyZAhCXP5o44wkmD5SVqyqoL5HmgNc8SJL7uMMMDDwecy7Sh9vvt3RXirH7F7bpUv3VsaepVGCHLfDp9GMG59ZiWK9Rmzf66e8Tw4unphu7gFNZuqeBk2YjCBj3i4eXbJvBEgCRB51FATRQY9JUzdMv9Mbkaq4DW69AgdqbES8aHeoax1UDDBi3raM8WpP2cKVEqoeeCGYM2vfN6zBAh7Tu3M4NcNFJmkNtd8Mpc2Md1kxRsusVzHiYxnsZjo"
     )
 
-    class FuncIdx(Ctrt.FuncIdx): 
+    class FuncIdx(Ctrt.FuncIdx):
         SUPERSEDE = 0
         ISSUE = 1
         DESTROY = 2
@@ -35,20 +36,20 @@ class TokenCtrtWithoutSplit(Ctrt):
         MAX_SUPPLY = 8
         BALANCE_OF = 9
         GET_ISSUER = 10
-    
-    
-    class StateVar(Ctrt.StateVar): 
+
+    class StateVar(Ctrt.StateVar):
         """
         StateVar is the enum class for state variables of a contract.
         """
+
         ISSUER = 0
         MAKER = 1
 
-    
     class DBKey(Ctrt.DBKey):
         """
         DBKey is the class for DB key of a contract used to query data.
         """
+
         @classmethod
         def for_issuer(cls) -> TokenCtrtWithoutSplit.DBKey:
             b = TokenCtrtWithoutSplit.StateVar.ISSUER.serialize()
@@ -58,7 +59,7 @@ class TokenCtrtWithoutSplit(Ctrt):
         def for_maker(cls) -> TokenCtrtWithoutSplit.DBKey:
             b = TokenCtrtWithoutSplit.StateVar.MAKER.serialize()
             return cls(b)
-         
+
     @classmethod
     async def register(
         cls,
@@ -91,21 +92,20 @@ class TokenCtrtWithoutSplit(Ctrt):
                     de.INT32(md.Int(unit)),
                     de.String(md.Str(token_description)),
                 ),
-            ctrt_meta=cls.CTRT_META,
-            timestamp=md.VSYSTimestamp.now(),
-            description=md.Str(ctrt_description),
-            fee=md.RegCtrtFee(fee),
+                ctrt_meta=cls.CTRT_META,
+                timestamp=md.VSYSTimestamp.now(),
+                description=md.Str(ctrt_description),
+                fee=md.RegCtrtFee(fee),
             )
-        )   
+        )
         logger.debug(data)
 
         return cls(
-            data["contractId"], 
-            chain = by.chain,
+            data["contractId"],
+            chain=by.chain,
         )
-        
-    @property
 
+    @property
     async def issuer(self) -> str:
         """
         issuer queries & returns the issuer of the contract.
@@ -135,15 +135,14 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data["value"]
 
-
     async def supersede(
         self,
         by: acnt.Account,
         new_issuer: str,
-        attachment: str= "",
+        attachment: str = "",
         fee: int = md.ExecCtrtFee.DEFAULT,
-    ) -> Dict[str,any]:
-        """ 
+    ) -> Dict[str, any]:
+        """
         Transfer the issuing right of the contract to another account
 
         Args:
@@ -158,11 +157,9 @@ class TokenCtrtWithoutSplit(Ctrt):
 
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
-                ctrt_id = self._ctrt_id,
-                func_id = self.FuncIdx.SUPERSEDE,
-                data_stack = de.DataStack(
-                    de.Addr(md.Addr(new_issuer))
-                ),
+                ctrt_id=self._ctrt_id,
+                func_id=self.FuncIdx.SUPERSEDE,
+                data_stack=de.DataStack(de.Addr(md.Addr(new_issuer))),
                 timestamp=md.VSYSTimestamp.now(),
                 attachment=md.Str(new_issuer),
                 fee=md.ExecCtrtFee(fee),
@@ -171,15 +168,14 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
     async def issue(
         self,
         by: acnt.Account,
         amount: int,
         attachment: str = "",
         fee: int = md.ExecCtrtFee.DEFAULT,
-    ) -> Dict[str,any]:
-        """ 
+    ) -> Dict[str, any]:
+        """
         Issue new Tokens by account who has the issuing right
 
         Args:
@@ -193,9 +189,9 @@ class TokenCtrtWithoutSplit(Ctrt):
         """
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
-                ctrt_id = self._ctrt_id,
-                func_id = self.FuncIdx.ISSUE,
-                data_stack = de.DataStack(
+                ctrt_id=self._ctrt_id,
+                func_id=self.FuncIdx.ISSUE,
+                data_stack=de.DataStack(
                     de.Long(md.Int(amount)),
                 ),
                 timestamp=md.VSYSTimestamp.now(),
@@ -205,17 +201,16 @@ class TokenCtrtWithoutSplit(Ctrt):
         )
         logger.debug(data)
         return data
-    
 
     async def send(
         self,
         by: acnt.Account,
         recipient: str,
         amount: int,
-        attachment: str= "",
+        attachment: str = "",
         fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
-        """ 
+        """
         send tokens to another account
 
         Args:
@@ -231,7 +226,6 @@ class TokenCtrtWithoutSplit(Ctrt):
 
         rcpt_md = md.Addr(recipient)
         rcpt_md.must_on(by.chain)
-
 
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
@@ -249,15 +243,14 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-        
-
-    async def destroy(self,
+    async def destroy(
+        self,
         by: acnt.Account,
         amount: int,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
-        """ 
+        """
         Destroy an amount of tokens by account who has the issuing right
 
         Args:
@@ -269,7 +262,6 @@ class TokenCtrtWithoutSplit(Ctrt):
         Returns:
             Dict[str, Any]: The response returned by the Node API
         """
-
 
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
@@ -286,16 +278,16 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def transfer(self,
+    async def transfer(
+        self,
         by: acnt.Account,
         sender: str,
         recipient: str,
         amount: int,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
-        """ 
+        """
         transfer tokens from sender to recipient
 
         Args:
@@ -316,7 +308,6 @@ class TokenCtrtWithoutSplit(Ctrt):
         sender_md.must_on(by.chain)
         rcpt_md.must_on(by.chain)
 
-
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
                 ctrt_id=self._ctrt_id,
@@ -334,19 +325,19 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def deposit(self,
-        sender : str,
-        contract : str,
+    async def deposit(
+        self,
+        by: acnt.Account,
+        contract: str,
         amount: int,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
         """
         deposit the tokens into the contract
 
         Args:
-            sender (str): The sender account
+            by (acnt.Account): The action maker.
             contract (str): The contract id to deposit into
             amount (int): The amount to deposit
             attachment (str, optional): The attachment of this action. Defaults to "".
@@ -356,11 +347,10 @@ class TokenCtrtWithoutSplit(Ctrt):
             Dict[str, Any]: The response returned by the Node API
         """
 
-        sender_md = md.Addr(sender)
+        sender_md = md.Addr(by.addr.b58_str)
         sender_md.must_on(by.chain)
 
-
-        data = await acnt.Account._execute_contract(
+        data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
                 ctrt_id=self._ctrt_id,
                 func_id=self.FuncIdx.DEPOSIT,
@@ -377,15 +367,15 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def withdraw(self,
-        contract : str,
-        recipient: str,
+    async def withdraw(
+        self,
+        by: acnt.Account,
+        contract: str,
         amount: int,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
-        """ 
+        """
         withdraw tokens from another contract
 
         Args:
@@ -399,11 +389,10 @@ class TokenCtrtWithoutSplit(Ctrt):
             Dict[str, Any]: The response returned by the Node API
         """
 
-        rcpt_md = md.Addr(recipient)
+        rcpt_md = md.Addr(by.addr.b58_str)
         rcpt_md.must_on(by.chain)
 
-
-        data = await acnt.Account._execute_contract(
+        data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
                 ctrt_id=self._ctrt_id,
                 func_id=self.FuncIdx.WITHDRAW,
@@ -420,13 +409,10 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def get_total_supply(self,
-        by: acnt.Account,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+    async def get_total_supply(
+        self, by: acnt.Account, attachment: str = "", fee: int = md.ExecCtrtFee.DEFAULT
     ) -> Dict[str, Any]:
-        """ 
+        """
         Get the total token supply of the contract
 
         Args:
@@ -438,13 +424,11 @@ class TokenCtrtWithoutSplit(Ctrt):
             Dict[str, Any]: The response returned by the Node API
         """
 
-
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
                 ctrt_id=self._ctrt_id,
                 func_id=self.FuncIdx.TOTAL_SUPPLY,
-                data_stack=de.DataStack(
-                ),
+                data_stack=de.DataStack(),
                 timestamp=md.VSYSTimestamp.now(),
                 attachment=md.Str(attachment),
                 fee=md.ExecCtrtFee(fee),
@@ -453,13 +437,10 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def get_max_supply(self,
-        by: acnt.Account,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+    async def get_max_supply(
+        self, by: acnt.Account, attachment: str = "", fee: int = md.ExecCtrtFee.DEFAULT
     ) -> Dict[str, Any]:
-        """ 
+        """
         Get the max supply of the contract
 
         Args:
@@ -471,14 +452,11 @@ class TokenCtrtWithoutSplit(Ctrt):
             Dict[str, Any]: The response returned by the Node API
         """
 
-
-
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
                 ctrt_id=self._ctrt_id,
                 func_id=self.FuncIdx.MAX_SUPPLY,
-                data_stack=de.DataStack(
-                ),
+                data_stack=de.DataStack(),
                 timestamp=md.VSYSTimestamp.now(),
                 attachment=md.Str(attachment),
                 fee=md.ExecCtrtFee(fee),
@@ -487,14 +465,14 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def get_balance_of(self,
+    async def get_balance_of(
+        self,
         by: acnt.Account,
         address: str,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
-        """ 
+        """
         Get the balance of the address
 
         Args:
@@ -506,7 +484,6 @@ class TokenCtrtWithoutSplit(Ctrt):
         Returns:
             Dict[str, Any]: The response returned by the Node API
         """
-
 
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
@@ -523,11 +500,8 @@ class TokenCtrtWithoutSplit(Ctrt):
         logger.debug(data)
         return data
 
-
-    async def get_issuer(self,
-        by: acnt.Account,
-        attachment: str= "",
-        fee: int = md.ExecCtrtFee.DEFAULT
+    async def get_issuer(
+        self, by: acnt.Account, attachment: str = "", fee: int = md.ExecCtrtFee.DEFAULT
     ) -> Dict[str, Any]:
         """
         Get the issuer account of the contract
@@ -541,13 +515,11 @@ class TokenCtrtWithoutSplit(Ctrt):
             Dict[str, Any]: The response returned by the Node API
         """
 
-
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
                 ctrt_id=self._ctrt_id,
                 func_id=self.FuncIdx.GET_ISSUER,
-                data_stack=de.DataStack(
-                ),
+                data_stack=de.DataStack(),
                 timestamp=md.VSYSTimestamp.now(),
                 attachment=md.Str(attachment),
                 fee=md.ExecCtrtFee(fee),
@@ -555,14 +527,15 @@ class TokenCtrtWithoutSplit(Ctrt):
         )
         logger.debug(data)
         return data
- 
+
+
 class TokenCtrtWithSplit(TokenCtrtWithoutSplit):
 
     CTRT_META = CtrtMeta.from_b58_str(
         "3dPGAWbTw4srh5hmMiRUhHtcxmXXLUooKGAfnmz11j5NruyJpBzZpgvADMdZS7Mevy5MAHqFbfHYdfqaAe1JEpLWt1pJWLHZBV62zUhLGmVLXUP5UDvSs24jsBRHqZMC71ciE1uYtgydKxCoFJ3rAgsYqp7GDeTU2PXS5ygDmL6WXmbAYPS8jE4sfNUbJVwpvL1cTw4nnjnJvmLET8VmQybxFt415RemV3MFPeYZay5i5gMmyZa63bjzK1uMZAVWA9TpF5YQ1NTZjPaRPvQGYVY4kY9L4LFJvUG2bib1QaNh7wUAQnTzJfRYJoy1aegFGFZFnBGp9GugH4fHAY69vGmZQnhDw3jU45G9odFyXo3T5Ww4R5szegbjCUKdUGpXf9vY2cKEMJ7i8eCkFVG1dDFZeVov1KMjkVNV8rDBDYfcp3oSGNWQQvGSUT5iGUvDRN8phy1UpR3A9uMVebvjLnVzPx9RyqQ8HaXLM8vPhLuWLoh5hk1Zi1n9nwz55XvKDYjP6eeB55yK5vpg8xjaYDnw9bjYV7ZmS7LAsHvXfnwi8y2W6vk2hGvs4rtR1vNRZSQMPGRRSuwCRJL1yngH6uHWwm2ajWxc684jApuoLdyjZomfCtdpabSyU3kp9Lrn8zT8BVY332sJPQU6gTQi8ke9s9dBxCae4cfSQM6HhuBmFc5KKWHCVG4bm4KZRYbMtidw8ZZnjaAMtcGq7k3Se6GXaTxdS3GcuttB3VB7njypyzuqAcfCdYb9ht8Y1WuTCZ1aLsXsL6eydfk2WLJVrqYpbTk6AchV5gMAEopvc3qXvzrDCedjtNsDmA56Lh6PxrrKr8aV8Wzz8aMaQ88YsVBpE8J4cDkxzo31AojhzEGVBKLmpb3bjmsaw9VkpB6yL8ngYs8eJMSPdM289TSMaEmG4eHt1jezpHTKxkuB9cwqcvhGNLWuv8KXQkik5pRMXV67Qs2FvjpzeJ81z2hnVh1wCtsa6M6qAG1gsqLHa1AVMRzsowafC99uDexwWMBS2RqsZWZBXJcUiNVULjApSnoBREYfHYEpjJ152hnTYZCAwpZMWEkVdBQpZ3zk8gbfLxB4fWMfKgJJucbKPGp1K56u7P8MHQu9aNb9dEof2mwX8rTHjk8jSQ7kXVX4Mf1JqMRWWftkV3GmU1nqYhxRGu4FjDNAomwTr5epHpcMF6P5oiXcLWh5BFQVmGYKz129oizAyUJBsZdxr2WZEGDieLxUg8cve25g28oTuCVENST4z1ZsFAN9wTa1"
     )
 
-    class FuncIdx(Ctrt.FuncIdx): 
+    class FuncIdx(Ctrt.FuncIdx):
         SUPERSEDE = 0
         ISSUE = 1
         DESTROY = 2
@@ -575,15 +548,15 @@ class TokenCtrtWithSplit(TokenCtrtWithoutSplit):
         MAXSUPPLY = 9
         BALANCEOF = 10
         GETISSUER = 11
-    
-    
-    async def split(self,
-    by: acnt.Account,
-    new_unit: int,
-    attachment: str= "",
-    fee: int = md.ExecCtrtFee.DEFAULT
+
+    async def split(
+        self,
+        by: acnt.Account,
+        new_unit: int,
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
     ) -> Dict[str, Any]:
-        """ 
+        """
         Update the unit
 
         Args:
@@ -595,7 +568,6 @@ class TokenCtrtWithSplit(TokenCtrtWithoutSplit):
         Returns:
             Dict[str, Any]: The response returned by the Node API
         """
-
 
         data = await by._execute_contract(
             tx.ExecCtrtFuncTxReq(
@@ -611,5 +583,3 @@ class TokenCtrtWithSplit(TokenCtrtWithoutSplit):
         )
         logger.debug(data)
         return data
-
-
