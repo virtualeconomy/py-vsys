ec71908d76e878ea3c7b091fc8b3de2a5aa63fe1
josephzxy <josephchuh7@gmail.com>
2022-01-26 11:58:05 +0800

    test: update fixtures

--- test/func_test/conftest.py
+++ test/func_test/conftest.py
@@ -32,38 +32,43 @@ def chain(api: pv.NodeAPI) -> pv.Chain:
 
 
 @pytest.fixture
-def seed() -> str:
-    return SEED
+def seed() -> pv.Seed:
+    return pv.Seed(SEED)
 
 
 @pytest.fixture
-def acnt0(chain: pv.Chain, seed: str) -> pv.Account:
+def wallet(seed: pv.Seed) -> pv.Wallet:
+    return pv.Wallet(seed)
+
+
+@pytest.fixture
+def acnt0(chain: pv.Chain, wallet: pv.Wallet) -> pv.Account:
     """
     acnt0 is the fixture that returns the account of nonce 0.
 
     Args:
         chain (pv.Chain): The Chain object.
-        seed (str): The account seed.
+        wallet (pv.Wallet): The wallet object.
 
     Returns:
         pv.Account: The account.
     """
-    return pv.Account(chain, seed, 0)
+    return wallet.get_account(chain, 0)
 
 
 @pytest.fixture
-def acnt1(chain: pv.Chain, seed: str) -> pv.Account:
+def acnt1(chain: pv.Chain, wallet: pv.Wallet) -> pv.Account:
     """
     acnt1 is the fixture that returns the account of nonce 1.
 
     Args:
         chain (pv.Chain): The Chain object.
-        seed (str): The account seed.
+        wallet (pv.Wallet): The wallet object.
 
     Returns:
         pv.Account: The account.
     """
-    return pv.Account(chain, seed, 1)
+    return wallet.get_account(chain, 1)
 
 
 async def wait_for_block() -> None:
