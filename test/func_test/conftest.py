e6c83e1ce0173affc27524b4efda951e135abe9c
josephzxy <josephchuh7@gmail.com>
2022-01-27 11:03:15 +0800

    test: add global var AVG_BLOCK_DELAY

--- test/func_test/conftest.py
+++ test/func_test/conftest.py
@@ -9,6 +9,7 @@ HOST = ""
 API_KEY = ""
 SEED = ""
 SUPERNODE_ADDR = ""
+AVG_BLOCK_DELAY = 6  # in seconds
 
 
 @pytest.fixture
@@ -77,7 +78,7 @@ async def wait_for_block() -> None:
     """
     wait_for_block waits for the transaction to be packed into a block.
     """
-    await asyncio.sleep(6)
+    await asyncio.sleep(AVG_BLOCK_DELAY)
 
 
 async def assert_tx_success(api: pv.NodeAPI, tx_id: str) -> None:
