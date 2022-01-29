ab86a30eefec8eb57c850d2756b719f398ae00b1
josephzxy <josephchuh7@gmail.com>
2022-01-29 18:02:28 +0800

    feat: add method swap_b_for_exact_a

--- py_v_sdk/contract/v_swap_ctrt.py
+++ py_v_sdk/contract/v_swap_ctrt.py
@@ -696,3 +696,49 @@ class VSwapCtrt(Ctrt):
         )
         logger.debug(data)
         return data
+
+    async def swap_b_for_exact_a(
+        self,
+        by: acnt.Account,
+        amount_a: Union[int, float],
+        amount_b_max: Union[int, float],
+        deadline: int,
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
+    ) -> Dict[str, Any]:
+        """
+        swap_b_for_exact_a swaps token B for token A where the desired amount of token A is fixed.
+
+        Args:
+            by (acnt.Account): The action taker.
+            amount_a (Union[int, float]): The desired amount of token A.
+            amount_b_max (Union[int, float]): The maximum amount of token B the action taker is willing to pay.
+            deadline (int): Unix timestamp. The deadline for this operation to complete.
+            attachment (str, optional): The attachment of this action. Defaults to "".
+            fee (int, optional): The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+
+        Returns:
+            Dict[str, Any]: The response returned by the Node API
+        """
+
+        tok_a_unit, tok_b_unit = await asyncio.gather(
+            self.tok_a_unit,
+            self.tok_b_unit,
+        )
+
+        data = await by._execute_contract(
+            tx.ExecCtrtFuncTxReq(
+                ctrt_id=self._ctrt_id,
+                func_id=self.FuncIdx.SWAP_B_FOR_EXACT_A,
+                data_stack=de.DataStack(
+                    de.Amount.for_tok_amount(amount_a, tok_a_unit),
+                    de.Amount.for_tok_amount(amount_b_max, tok_b_unit),
+                    de.Timestamp(md.VSYSTimestamp.from_unix_ts(deadline)),
+                ),
+                timestamp=md.VSYSTimestamp.now(),
+                attachment=md.Str(attachment),
+                fee=md.ExecCtrtFee(fee),
+            )
+        )
+        logger.debug(data)
+        return data
