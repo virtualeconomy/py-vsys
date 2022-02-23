2e0e26cd8eb1e0a99df14fecfc9f4591d56f7405
josephzxy <josephchuh7@gmail.com>
2022-02-23 17:09:41 +0800

    test: add test cases for method deposit & withdraw

--- test/func_test/test_ctrt/test_sys_ctrt.py
+++ test/func_test/test_ctrt/test_sys_ctrt.py
@@ -22,6 +22,28 @@ class TestSysCtrt:
         """
         return pv.SysCtrt.for_testnet(acnt0.chain)
 
+    @pytest.fixture
+    async def new_pay_chan_ctrt(
+        self,
+        acnt0: pv.Account,
+        new_ctrt: pv.SysCtrt,
+    ) -> pv.PayChanCtrt:
+        """
+        new_pay_chan_ctrt is the fixture that creates a representative object for the
+        Payment Channel contract.
+
+        Args:
+            acnt0 (pv.Account): The account of nonce 0.
+            new_ctrt (pv.SysCtrt): The system contract.
+
+        Returns:
+            pv.PayChanCtrt: The Payment Channel contract instance.
+        """
+        sc = new_ctrt
+        pc = await pv.PayChanCtrt.register(acnt0, sc.tok_id)
+        await cft.wait_for_block()
+        return pc
+
     async def test_send(
         self,
         new_ctrt: pv.SysCtrt,
@@ -90,10 +112,58 @@ class TestSysCtrt:
         )
         assert acnt1_bal == acnt1_bal_old + pv.VSYS.for_amount(amount).data
 
+    async def test_deposit_and_withdraw(
+        self,
+        acnt0: pv.Account,
+        new_ctrt: pv.SysCtrt,
+        new_pay_chan_ctrt: pv.PayChanCtrt,
+    ) -> None:
+        """
+        test_deposit_and_withdraw tests methods
+            - deposit
+            - withdraw
+
+        Args:
+            acnt0 (pv.Account): The account of nonce 0.
+            new_ctrt (pv.SysCtrt): The fixture that creates a representative object for the System contract.
+            new_pay_chan_ctrt (pv.PayChanCtrt): The fixture that creates a representative object for the Payment Channel contract.
+        """
+        api = acnt0.api
+        sc = new_ctrt
+        pc = new_pay_chan_ctrt
+        deposit_amount = 1
+
+        bal_init = await acnt0.balance
+
+        resp = await sc.deposit(acnt0, pc.ctrt_id, deposit_amount)
+        await cft.wait_for_block()
+        await cft.assert_tx_success(api, resp["id"])
+
+        bal_after_deposit = await acnt0.balance
+        assert (
+            bal_after_deposit
+            == bal_init
+            - pv.VSYS.for_amount(deposit_amount).data
+            - pv.ExecCtrtFee.DEFAULT
+        )
+
+        resp = await sc.withdraw(acnt0, pc.ctrt_id, deposit_amount)
+        await cft.wait_for_block()
+        await cft.assert_tx_success(api, resp["id"])
+
+        bal_after_withdraw = await acnt0.balance
+        assert (
+            bal_after_withdraw
+            == bal_after_deposit
+            + pv.VSYS.for_amount(deposit_amount).data
+            - pv.ExecCtrtFee.DEFAULT
+        )
+
     @pytest.mark.whole
     async def test_as_whole(
         self,
         new_ctrt: pv.SysCtrt,
+        new_pay_chan_ctrt: pv.PayChanCtrt,
         acnt0: pv.Account,
         acnt1: pv.Account,
     ):
@@ -104,9 +174,13 @@ class TestSysCtrt:
 
         Args:
             new_ctrt (pv.SysCtrt): The fixture that creates a representative object for the System contract.
+            new_pay_chan_ctrt (pv.PayChanCtrt): The fixture that creates a representative object for the Payment Channel contract.
             acnt0 (pv.Account): The account of nonce 0.
             acnt1 (pv.Account): The account of nonce 1.
         """
         sc = new_ctrt
         await self.test_send(sc, acnt0, acnt1)
         await self.test_transfer(sc, acnt1, acnt0)
+
+        pc = new_pay_chan_ctrt
+        await self.test_deposit_and_withdraw(acnt0, sc, pc)
