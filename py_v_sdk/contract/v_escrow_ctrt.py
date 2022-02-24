de11028637fc707d43245211286d789464e3c62e
josephzxy <josephchuh7@gmail.com>
2022-02-24 15:04:02 +0800

    feat: add method judge_deposit

--- py_v_sdk/contract/v_escrow_ctrt.py
+++ py_v_sdk/contract/v_escrow_ctrt.py
@@ -981,3 +981,38 @@ class VEscrowCtrt(Ctrt):
         )
         logger.debug(data)
         return data
+
+    async def judge_deposit(
+        self,
+        by: acnt.Account,
+        order_id: str,
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
+    ) -> Dict[str, Any]:
+        """
+        judge_deposit deposits tokens the judge deposited into the contract into the order.
+
+        Args:
+            by (acnt.Account): The action taker.
+            order_id (str): The order ID.
+            attachment (str, optional): The attachment of this action. Defaults to "".
+            fee (int, optional): The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+
+        Returns:
+            Dict[str, Any]: The response returned by the Node API.
+        """
+
+        data = await by._execute_contract(
+            tx.ExecCtrtFuncTxReq(
+                ctrt_id=self._ctrt_id,
+                func_id=self.FuncIdx.JUDGE_DEPOSIT,
+                data_stack=de.DataStack(
+                    de.Bytes.for_base58_str(order_id),
+                ),
+                timestamp=md.VSYSTimestamp.now(),
+                attachment=md.Str(attachment),
+                fee=md.ExecCtrtFee(fee),
+            )
+        )
+        logger.debug(data)
+        return data
