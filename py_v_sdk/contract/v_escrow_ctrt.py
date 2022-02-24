3743c511c3b98883b220c514f53a8d7855c3a558
josephzxy <josephchuh7@gmail.com>
2022-02-24 15:09:32 +0800

    feat: add method submit_work

--- py_v_sdk/contract/v_escrow_ctrt.py
+++ py_v_sdk/contract/v_escrow_ctrt.py
@@ -1121,3 +1121,38 @@ class VEscrowCtrt(Ctrt):
         )
         logger.debug(data)
         return data
+
+    async def submit_work(
+        self,
+        by: acnt.Account,
+        order_id: str,
+        attachment: str = "",
+        fee: int = md.ExecCtrtFee.DEFAULT,
+    ) -> Dict[str, Any]:
+        """
+        submit_work submits the work by the recipient.
+
+        Args:
+            by (acnt.Account): The action taker.
+            order_id (str): The order ID.
+            attachment (str, optional): The attachment of this action. Defaults to "".
+            fee (int, optional): The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+
+        Returns:
+            Dict[str, Any]: The response returned by the Node API.
+        """
+
+        data = await by._execute_contract(
+            tx.ExecCtrtFuncTxReq(
+                ctrt_id=self._ctrt_id,
+                func_id=self.FuncIdx.SUBMIT_WORK,
+                data_stack=de.DataStack(
+                    de.Bytes.for_base58_str(order_id),
+                ),
+                timestamp=md.VSYSTimestamp.now(),
+                attachment=md.Str(attachment),
+                fee=md.ExecCtrtFee(fee),
+            )
+        )
+        logger.debug(data)
+        return data
