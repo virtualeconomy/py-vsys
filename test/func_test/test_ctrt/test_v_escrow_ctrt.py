64e531525bbe66843e84c280a42f9d5aadb73035
josephzxy <josephchuh7@gmail.com>
2022-02-25 15:53:04 +0800

    test: add test case class with fixtures for V Escrow contract

---
+++ test/func_test/test_ctrt/test_v_escrow_ctrt.py
@@ -0,0 +1,107 @@
+import pytest
+
+import py_v_sdk as pv
+from test.func_test import conftest as cft
+
+
+class TestVEscrowCtrt:
+    """
+    TestVEscrowCtrt is the collection of functional tests of V Escrow Contract.
+    """
+
+    TOK_MAX = 100
+    TOK_UNIT = 1
+    INIT_LOAD = TOK_MAX // 2
+
+    @pytest.fixture
+    async def new_sys_ctrt(self, chain: pv.Chain) -> pv.SysCtrt:
+        """
+        new_sys_ctrt is the fixture that returns a system contract instance.
+
+        Args:
+            chain (pv.Chain): The chain object.
+
+        Returns:
+            pv.SysCtrt: The system contract instance.
+        """
+        return pv.SysCtrt.for_testnet(chain)
+
+    async def _new_ctrt(
+        self,
+        new_sys_ctrt: pv.SysCtrt,
+        acnt0: pv.Account,
+        duration: int,
+    ) -> pv.VEscrowCtrt:
+        """
+        _new_ctrt registers a new V Escrow Contract where the payer duration & judge duration
+        are all the given duration.
+
+        Args:
+            new_sys_ctrt (pv.SysCtrt): The system contract instance.
+            acnt0 (pv.Account): The account of nonce 0.
+            duration (int): The duration in seconds.
+
+        Returns:
+            pv.VEscrowCtrt: The VEscrowCtrt instance.
+        """
+        sc = new_sys_ctrt
+
+        vc = await pv.VEscrowCtrt.register(
+            by=acnt0,
+            tok_id=sc.tok_id,
+            duration=duration,
+            judge_duration=duration,
+        )
+        await cft.wait_for_block()
+        return vc
+
+    @pytest.fixture
+    async def new_ctrt_with_ten_mins_duration(
+        self,
+        new_sys_ctrt: pv.SysCtrt,
+        acnt0: pv.Account,
+    ) -> pv.VEscrowCtrt:
+        """
+        new_ctrt_with_ten_mins_duration is the fixture that registers
+        a new V Escrow Contract where the payer duration & judge duration
+        are all 10 mins
+
+        Args:
+            new_sys_ctrt (pv.SysCtrt): The system contract instance.
+            acnt0 (pv.Account): The account of nonce 0.
+
+        Returns:
+            pv.VEscrowCtrt: The VEscrowCtrt instance.
+        """
+        ten_mins = 10 * 60
+        return await self._new_ctrt(
+            new_sys_ctrt,
+            acnt0,
+            ten_mins,
+        )
+
+    @pytest.fixture
+    async def new_ctrt_with_five_secs_duration(
+        self,
+        new_sys_ctrt: pv.SysCtrt,
+        acnt0: pv.Account,
+    ) -> pv.VEscrowCtrt:
+        """
+        new_ctrt_with_ten_mins_duration is the fixture that registers
+        a new V Escrow Contract where the payer duration & judge duration
+        are all 5 secs.
+
+        Args:
+            new_sys_ctrt (pv.SysCtrt): The system contract instance.
+            acnt0 (pv.Account): The account of nonce 0.
+
+        Returns:
+            pv.VEscrowCtrt: The VEscrowCtrt instance.
+        """
+        five_secs = 5
+        return await self._new_ctrt(
+            new_sys_ctrt,
+            acnt0,
+            five_secs,
+        )
+
