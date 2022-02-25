865c7fc9c7a5ff4bbd9da0505c33f1fd9a94f2df
josephzxy <josephchuh7@gmail.com>
2022-02-25 18:18:04 +0800

    test: add account fixtures

--- test/func_test/test_ctrt/test_v_escrow_ctrt.py
+++ test/func_test/test_ctrt/test_v_escrow_ctrt.py
@@ -1,3 +1,6 @@
+import asyncio
+import time
+
 import pytest
 
 import py_v_sdk as pv
@@ -9,9 +12,64 @@ class TestVEscrowCtrt:
     TestVEscrowCtrt is the collection of functional tests of V Escrow Contract.
     """
 
-    TOK_MAX = 100
-    TOK_UNIT = 1
-    INIT_LOAD = TOK_MAX // 2
+    ORDER_AMOUNT = 10
+    RCPT_DEPOSIT_AMOUNT = 2
+    JUDGE_DEPOSIT_AMOUNT = 3
+    ORDER_FEE = 4
+    REFUND_AMOUNT = 5
+    CTRT_DEPOSIT_AMOUNT = 30
+
+    @pytest.fixture
+    def maker(self, acnt0: pv.Account) -> pv.Account:
+        """
+        maker is the fixture that returns the maker account used in the tests.
+
+        Args:
+            acnt0 (pv.Account): The account of nonce 0.
+
+        Returns:
+            pv.Account: The account.
+        """
+        return acnt0
+
+    @pytest.fixture
+    def judge(self, acnt0: pv.Account) -> pv.Account:
+        """
+        judge is the fixture that returns the judge account used in the tests.
+
+        Args:
+            acnt0 (pv.Account): The account of nonce 0.
+
+        Returns:
+            pv.Account: The account.
+        """
+        return acnt0
+
+    @pytest.fixture
+    def payer(self, acnt1: pv.Account) -> pv.Account:
+        """
+        payer is the fixture that returns the payer account used in the tests.
+
+        Args:
+            acnt0 (pv.Account): The account of nonce 0.
+
+        Returns:
+            pv.Account: The account.
+        """
+        return acnt1
+
+    @pytest.fixture
+    def recipient(self, acnt2: pv.Account) -> pv.Account:
+        """
+        recipient is the fixture that returns the recipient account used in the tests.
+
+        Args:
+            acnt0 (pv.Account): The account of nonce 0.
+
+        Returns:
+            pv.Account: The account.
+        """
+        return acnt2
 
     @pytest.fixture
     async def new_sys_ctrt(self, chain: pv.Chain) -> pv.SysCtrt:
@@ -29,7 +87,10 @@ class TestVEscrowCtrt:
     async def _new_ctrt(
         self,
         new_sys_ctrt: pv.SysCtrt,
-        acnt0: pv.Account,
+        maker: pv.Account,
+        judge: pv.Account,
+        payer: pv.Account,
+        recipient: pv.Account,
         duration: int,
     ) -> pv.VEscrowCtrt:
         """
@@ -38,28 +99,48 @@ class TestVEscrowCtrt:
 
         Args:
             new_sys_ctrt (pv.SysCtrt): The system contract instance.
-            acnt0 (pv.Account): The account of nonce 0.
+            maker (pv.Account): The account of the contract maker.
+            judge (pv.Account): The account of the contract judge.
+            payer (pv.Account): The account of the contract payer.
+            recipient (pv.Account): The account of the contract recipient.
             duration (int): The duration in seconds.
 
         Returns:
             pv.VEscrowCtrt: The VEscrowCtrt instance.
         """
         sc = new_sys_ctrt
+        api = maker.api
 
         vc = await pv.VEscrowCtrt.register(
-            by=acnt0,
+            by=maker,
             tok_id=sc.tok_id,
             duration=duration,
             judge_duration=duration,
         )
         await cft.wait_for_block()
+
+        judge_resp, payer_resp, rcpt_resp = await asyncio.gather(
+            sc.deposit(judge, vc.ctrt_id, self.CTRT_DEPOSIT_AMOUNT),
+            sc.deposit(payer, vc.ctrt_id, self.CTRT_DEPOSIT_AMOUNT),
+            sc.deposit(recipient, vc.ctrt_id, self.CTRT_DEPOSIT_AMOUNT),
+        )
+        await cft.wait_for_block()
+
+        await asyncio.gather(
+            cft.assert_tx_success(api, judge_resp["id"]),
+            cft.assert_tx_success(api, payer_resp["id"]),
+            cft.assert_tx_success(api, rcpt_resp["id"]),
+        )
         return vc
 
     @pytest.fixture
     async def new_ctrt_with_ten_mins_duration(
         self,
         new_sys_ctrt: pv.SysCtrt,
-        acnt0: pv.Account,
+        maker: pv.Account,
+        judge: pv.Account,
+        payer: pv.Account,
+        recipient: pv.Account,
     ) -> pv.VEscrowCtrt:
         """
         new_ctrt_with_ten_mins_duration is the fixture that registers
@@ -68,7 +149,10 @@ class TestVEscrowCtrt:
 
         Args:
             new_sys_ctrt (pv.SysCtrt): The system contract instance.
-            acnt0 (pv.Account): The account of nonce 0.
+            maker (pv.Account): The account of the contract maker.
+            judge (pv.Account): The account of the contract judge.
+            payer (pv.Account): The account of the contract payer.
+            recipient (pv.Account): The account of the contract recipient.
 
         Returns:
             pv.VEscrowCtrt: The VEscrowCtrt instance.
@@ -76,7 +160,10 @@ class TestVEscrowCtrt:
         ten_mins = 10 * 60
         return await self._new_ctrt(
             new_sys_ctrt,
-            acnt0,
+            maker,
+            judge,
+            payer,
+            recipient,
             ten_mins,
         )
 
@@ -84,7 +171,10 @@ class TestVEscrowCtrt:
     async def new_ctrt_with_five_secs_duration(
         self,
         new_sys_ctrt: pv.SysCtrt,
-        acnt0: pv.Account,
+        maker: pv.Account,
+        judge: pv.Account,
+        payer: pv.Account,
+        recipient: pv.Account,
     ) -> pv.VEscrowCtrt:
         """
         new_ctrt_with_ten_mins_duration is the fixture that registers
@@ -93,7 +183,10 @@ class TestVEscrowCtrt:
 
         Args:
             new_sys_ctrt (pv.SysCtrt): The system contract instance.
-            acnt0 (pv.Account): The account of nonce 0.
+            maker (pv.Account): The account of the contract maker.
+            judge (pv.Account): The account of the contract judge.
+            payer (pv.Account): The account of the contract payer.
+            recipient (pv.Account): The account of the contract recipient.
 
         Returns:
             pv.VEscrowCtrt: The VEscrowCtrt instance.
@@ -101,7 +194,10 @@ class TestVEscrowCtrt:
         five_secs = 5
         return await self._new_ctrt(
             new_sys_ctrt,
-            acnt0,
+            maker,
+            judge,
+            payer,
+            recipient,
             five_secs,
         )
 
@@ -109,7 +205,7 @@ class TestVEscrowCtrt:
         self,
         new_sys_ctrt: pv.SysCtrt,
         new_ctrt_with_ten_mins_duration: pv.VEscrowCtrt,
-        acnt0: pv.Account,
+        maker: pv.Account,
     ) -> pv.VEscrowCtrt:
         """
         test_register tests the method register.
@@ -117,7 +213,7 @@ class TestVEscrowCtrt:
         Args:
             new_sys_ctrt (pv.SysCtrt): The system contract instance.
             new_ctrt_with_ten_mins_duration (pv.VEscrowCtrt): The V Escrow contract instance.
-            acnt0 (pv.Account): The account of nonce 0.
+            maker (pv.Account): The account of the contract maker.
 
         Returns:
             pv.VEscrowCtrt: The VEscrowCtrt instance.
@@ -126,11 +222,8 @@ class TestVEscrowCtrt:
         sc = new_sys_ctrt
         vc = new_ctrt_with_ten_mins_duration
 
-        maker = await vc.maker
-        assert maker.data == acnt0.addr.b58_str
-
-        judge = await vc.judge
-        assert judge.data == acnt0.addr.b58_str
+        assert (await vc.maker).data == maker.addr.b58_str
+        assert (await vc.judge).data == maker.addr.b58_str
 
         tok_id = await vc.tok_id
         assert tok_id.data == sc.tok_id
