7e0a03af7c34419059972c2b028a7b945f91b9d3
josephzxy <josephchuh7@gmail.com>
2022-02-28 10:49:43 +0800

    test: rename fixtures

--- test/func_test/test_ctrt/test_v_escrow_ctrt.py
+++ test/func_test/test_ctrt/test_v_escrow_ctrt.py
@@ -135,7 +135,7 @@ class TestVEscrowCtrt:
         return vc
 
     @pytest.fixture
-    async def new_ctrt_ten_mins_duration(
+    async def new_ctrt_ten_mins(
         self,
         new_sys_ctrt: pv.SysCtrt,
         maker: pv.Account,
@@ -144,7 +144,7 @@ class TestVEscrowCtrt:
         recipient: pv.Account,
     ) -> pv.VEscrowCtrt:
         """
-        new_ctrt_ten_mins_duration is the fixture that registers
+        new_ctrt_ten_mins is the fixture that registers
         a new V Escrow Contract where the payer duration & judge duration
         are all 10 mins
 
@@ -169,7 +169,7 @@ class TestVEscrowCtrt:
         )
 
     @pytest.fixture
-    async def new_ctrt_five_secs_duration(
+    async def new_ctrt_five_secs(
         self,
         new_sys_ctrt: pv.SysCtrt,
         maker: pv.Account,
@@ -178,7 +178,7 @@ class TestVEscrowCtrt:
         recipient: pv.Account,
     ) -> pv.VEscrowCtrt:
         """
-        new_ctrt_ten_mins_duration is the fixture that registers
+        new_ctrt_ten_mins is the fixture that registers
         a new V Escrow Contract where the payer duration & judge duration
         are all 5 secs.
 
@@ -203,26 +203,26 @@ class TestVEscrowCtrt:
         )
 
     @pytest.fixture
-    async def new_ctrt_ten_mins_duration_order(
+    async def new_ctrt_ten_mins_order(
         self,
-        new_ctrt_ten_mins_duration: pv.VEscrowCtrt,
+        new_ctrt_ten_mins: pv.VEscrowCtrt,
         payer: pv.Account,
         recipient: pv.Account,
     ) -> Tuple[pv.VEscrowCtrt, str]:
         """
-        new_ctrt_ten_mins_duration_order is the fixture that registers
+        new_ctrt_ten_mins_order is the fixture that registers
         a new V Escrow Contract where the payer duration & judge duration
         are all 10 mins with an order created.
 
         Args:
-            new_ctrt_ten_mins_duration (pv.VEscrowCtrt): The V Escrow contract instance.
+            new_ctrt_ten_mins (pv.VEscrowCtrt): The V Escrow contract instance.
             payer (pv.Account): The account of the contract payer.
             recipient (pv.Account): The account of the contract recipient.
 
         Returns:
             pv.VEscrowCtrt: The VEscrowCtrt instance.
         """
-        vc = new_ctrt_ten_mins_duration
+        vc = new_ctrt_ten_mins
         api = payer.api
         a_day_later = int(time.time()) + 60 * 60 * 24
 
@@ -245,7 +245,7 @@ class TestVEscrowCtrt:
     async def test_register(
         self,
         new_sys_ctrt: pv.SysCtrt,
-        new_ctrt_ten_mins_duration: pv.VEscrowCtrt,
+        new_ctrt_ten_mins: pv.VEscrowCtrt,
         maker: pv.Account,
     ) -> pv.VEscrowCtrt:
         """
@@ -253,7 +253,7 @@ class TestVEscrowCtrt:
 
         Args:
             new_sys_ctrt (pv.SysCtrt): The system contract instance.
-            new_ctrt_ten_mins_duration (pv.VEscrowCtrt): The V Escrow contract instance.
+            new_ctrt_ten_mins (pv.VEscrowCtrt): The V Escrow contract instance.
             maker (pv.Account): The account of the contract maker.
 
         Returns:
@@ -261,7 +261,7 @@ class TestVEscrowCtrt:
         """
 
         sc = new_sys_ctrt
-        vc = new_ctrt_ten_mins_duration
+        vc = new_ctrt_ten_mins
 
         assert (await vc.maker).data == maker.addr.b58_str
         assert (await vc.judge).data == maker.addr.b58_str
@@ -280,7 +280,7 @@ class TestVEscrowCtrt:
 
     async def test_supersede(
         self,
-        new_ctrt_ten_mins_duration: pv.VEscrowCtrt,
+        new_ctrt_ten_mins: pv.VEscrowCtrt,
         acnt0: pv.Account,
         acnt1: pv.Account,
     ) -> pv.VEscrowCtrt:
@@ -288,7 +288,7 @@ class TestVEscrowCtrt:
         test_supersede tests the method supersede
 
         Args:
-            new_ctrt_ten_mins_duration (pv.VEscrowCtrt): The V Escrow contract instance.
+            new_ctrt_ten_mins (pv.VEscrowCtrt): The V Escrow contract instance.
             acnt0 (pv.Account): The account of nonce 0.
             acnt1 (pv.Account): The account of nonce 1.
 
@@ -296,7 +296,7 @@ class TestVEscrowCtrt:
             pv.VEscrowCtrt: The VEscrowCtrt instance.
         """
 
-        vc = new_ctrt_ten_mins_duration
+        vc = new_ctrt_ten_mins
         api = acnt0.api
 
         judge = await vc.judge
@@ -311,7 +311,7 @@ class TestVEscrowCtrt:
 
     async def test_create(
         self,
-        new_ctrt_ten_mins_duration: pv.VEscrowCtrt,
+        new_ctrt_ten_mins: pv.VEscrowCtrt,
         judge: pv.Account,
         payer: pv.Account,
         recipient: pv.Account,
@@ -320,13 +320,13 @@ class TestVEscrowCtrt:
         test_create tests the method create.
 
         Args:
-            new_ctrt_ten_mins_duration (pv.VEscrowCtrt): The V Escrow contract instance.
+            new_ctrt_ten_mins (pv.VEscrowCtrt): The V Escrow contract instance.
             maker (pv.Account): The account of the contract maker.
             payer (pv.Account): The account of the contract payer.
             recipient (pv.Account): The account of the contract recipient.
         """
 
-        vc = new_ctrt_ten_mins_duration
+        vc = new_ctrt_ten_mins
         api = judge.api
         a_day_later = int(time.time()) + 60 * 60 * 24
 
@@ -377,18 +377,18 @@ class TestVEscrowCtrt:
 
     async def test_recipient_deposit(
         self,
-        new_ctrt_ten_mins_duration_order: Tuple[pv.VEscrowCtrt, str],
+        new_ctrt_ten_mins_order: Tuple[pv.VEscrowCtrt, str],
         recipient: pv.Account,
     ) -> None:
         """
         test_recipient_deposit tests the method recipient_deposit.
 
         Args:
-            new_ctrt_ten_mins_duration_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
+            new_ctrt_ten_mins_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
                 where the payer duration & judge duration are all 10 mins and an order has been created.
             recipient (pv.Account): The account of the contract recipient.
         """
-        vc, order_id = new_ctrt_ten_mins_duration_order
+        vc, order_id = new_ctrt_ten_mins_order
         api = recipient.api
 
         assert (await vc.get_order_recipient_deposit_status(order_id)) is False
@@ -405,18 +405,18 @@ class TestVEscrowCtrt:
 
     async def test_judge_deposit(
         self,
-        new_ctrt_ten_mins_duration_order: Tuple[pv.VEscrowCtrt, str],
+        new_ctrt_ten_mins_order: Tuple[pv.VEscrowCtrt, str],
         judge: pv.Account,
     ) -> None:
         """
         test_judge_deposit tests the method judge_deposit.
 
         Args:
-            new_ctrt_ten_mins_duration_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
+            new_ctrt_ten_mins_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
                 where the payer duration & judge duration are all 10 mins and an order has been created.
             judge (pv.Account): The account of the contract judge.
         """
-        vc, order_id = new_ctrt_ten_mins_duration_order
+        vc, order_id = new_ctrt_ten_mins_order
         api = judge.api
 
         assert (await vc.get_order_judge_deposit_status(order_id)) is False
@@ -433,19 +433,19 @@ class TestVEscrowCtrt:
 
     async def test_payer_cancel(
         self,
-        new_ctrt_ten_mins_duration_order: Tuple[pv.VEscrowCtrt, str],
+        new_ctrt_ten_mins_order: Tuple[pv.VEscrowCtrt, str],
         payer: pv.Account,
     ) -> None:
         """
         test_payer_cancel tests the method payer_cancel.
 
         Args:
-            new_ctrt_ten_mins_duration_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
+            new_ctrt_ten_mins_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
                 where the payer duration & judge duration are all 10 mins and an order has been created.
             payer (pv.Account): The account of the contract payer.
         """
 
-        vc, order_id = new_ctrt_ten_mins_duration_order
+        vc, order_id = new_ctrt_ten_mins_order
         api = payer.api
 
         assert (await vc.get_order_status(order_id)) is True
@@ -458,19 +458,19 @@ class TestVEscrowCtrt:
 
     async def test_recipient_cancel(
         self,
-        new_ctrt_ten_mins_duration_order: Tuple[pv.VEscrowCtrt, str],
+        new_ctrt_ten_mins_order: Tuple[pv.VEscrowCtrt, str],
         recipient: pv.Account,
     ) -> None:
         """
         test_recipient_cancel tests the method recipient_cancel.
 
         Args:
-            new_ctrt_ten_mins_duration_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
+            new_ctrt_ten_mins_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
                 where the payer duration & judge duration are all 10 mins and an order has been created.
             recipient (pv.Account): The account of the contract recipient.
         """
 
-        vc, order_id = new_ctrt_ten_mins_duration_order
+        vc, order_id = new_ctrt_ten_mins_order
         api = recipient.api
 
         assert (await vc.get_order_status(order_id)) is True
@@ -483,19 +483,19 @@ class TestVEscrowCtrt:
 
     async def test_judge_cancel(
         self,
-        new_ctrt_ten_mins_duration_order: Tuple[pv.VEscrowCtrt, str],
+        new_ctrt_ten_mins_order: Tuple[pv.VEscrowCtrt, str],
         judge: pv.Account,
     ) -> None:
         """
         test_judge_cancel tests the method judge_cancel.
 
         Args:
-            new_ctrt_ten_mins_duration_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
+            new_ctrt_ten_mins_order (Tuple[pv.VEscrowCtrt, str]): The V Escrow contract instance
                 where the payer duration & judge duration are all 10 mins and an order has been created.
             judge (pv.Account): The account of the contract judge.
         """
 
-        vc, order_id = new_ctrt_ten_mins_duration_order
+        vc, order_id = new_ctrt_ten_mins_order
         api = judge.api
 
         assert (await vc.get_order_status(order_id)) is True
