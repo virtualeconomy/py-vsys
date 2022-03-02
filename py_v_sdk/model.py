795995e303dfc27b552b7f0d68db878e3e53a550
josephzxy <josephchuh7@gmail.com>
2022-03-02 13:24:09 +0800

    feat: add __eq__ for model

--- py_v_sdk/model.py
+++ py_v_sdk/model.py
@@ -43,6 +43,9 @@ class Model(abc.ABC):
         cls_name = self.__class__.__name__
         return f"{cls_name}({self.data})"
 
+    def __eq__(self, other: Model) -> bool:
+        return self.__class__ == other.__class__ and self.data == other.data
+
 
 class Str(Model):
     """
